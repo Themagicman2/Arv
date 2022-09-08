246103b248acdedc90380d0e776f14591f44876b
japo04001 <89467411+japo04001@users.noreply.github.com>
2022-09-08 12:28:28 +0200

    Add files via upload

---
+++ programering/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Rectangle {
+    protected int width;
+    protected int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
---
+++ programering/Triangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Triangle {
+    protected int width;
+    protected int height;
+
+    public Triangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
